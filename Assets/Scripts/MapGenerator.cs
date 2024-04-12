e8136e5632711697a9dd077b44e395510cd4ccdf
Rafael Pereira <contato@rafael-pereira.com>
2024-04-12 14:46:16 -0300

    MeshGenerator
    MeshGenerator funcionando corretamente

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 
 public class MapGenerator : MonoBehaviour
 {
-    public enum DrawMode { NoiseMap, ColourMap };
+    public enum DrawMode { NoiseMap, ColourMap, mesh };
     public DrawMode drawMode;
     public int mapWidth;
     public int mapHeight;
@@ -49,6 +49,10 @@ public class MapGenerator : MonoBehaviour
         else if (drawMode == DrawMode.ColourMap)
         {
            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
+        else if (drawMode == DrawMode.mesh)
+        {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
         }
     }
 
--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 
 public static class MeshGenerator
 {
-    public static void GenerateTerrainMesh(float[,] heightMap){
+    public static MeshData GenerateTerrainMesh(float[,] heightMap){
         int width = heightMap.GetLength(0);
         int height = heightMap.GetLength(1);
         float topLeftX = (width - 1) / -2f;
@@ -19,18 +20,27 @@ public static class MeshGenerator
             for (int x = 0; x < width; x++)
             {
                 meshData.vertices[vertexIndex] = new Vector3(x, heightMap[x, y], y);
+                meshData.uvs[vertexIndex] = new Vector2(x / (float)width, y / (float)height);
+                if (x < width - 1 && y < height - 1)
+                {
+                    meshData.AddTriangle(vertexIndex, vertexIndex + width + 1, vertexIndex + width);
+                    meshData.AddTriangle(vertexIndex + width + 1, vertexIndex, vertexIndex + 1);
+                }
                 vertexIndex++;
             }
         }
+        return meshData;
     }
 
     public class MeshData{
         public Vector3[] vertices;
         public int[] triangles;
         int triangleIndex;
+        public Vector2[] uvs;
 
         public MeshData(int meshWidth, int meshHeight){
             vertices = new Vector3[meshWidth * meshHeight];
+            uvs = new Vector2[meshWidth * meshHeight];
             triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
         }
         public void AddTriangle(int a, int b, int c){
@@ -38,6 +48,14 @@ public static class MeshGenerator
             triangles[triangleIndex + 1] = b;
             triangles[triangleIndex + 2] = c;
             triangleIndex += 3;
+        }
+        public Mesh CreateMesh(){
+            Mesh mesh = new Mesh();
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+            mesh.uv = uvs;
+            mesh.RecalculateNormals();
+            return mesh;
         }
     }
 
